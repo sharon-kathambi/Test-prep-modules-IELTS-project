28d5be9cb8af1aa1f754e9cf1a7223d566e2c3cf
Sharon Mwenda <smkathambi@gmail.com>
2023-02-24 11:48:09 +0300

    feat[sidebar]: adding sidebar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
+import { useState } from 'react';
 import { ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import Dashboard from './pages/dashboard/Dashboard';
+import Sidebar from './components/global/sidebar/Sidebar';
+import Topbar from './components/global/topbar/Topbar';
 
 function App() {
   const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar isSidebar={isSidebar}/>
           <main className='content'>
-          <Dashboard />
+            <Topbar setIsSidebar={setIsSidebar} />
           </main>
         </div>
       </ThemeProvider>
